45f4db8f10cbf51a5200aca7b6436654fc73e2a1
Aqdas Amir <aqdasamir333@gmail.com>
2026-03-26 23:42:29 +0530

    Added the signup controller

---
+++ backend/src/lib/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+export const connectDB = async () => {
+    try{
+        const { MONGO_URI } = process.env;
+        if (!MONGO_URI) throw new Error("MONGO_URI is not set");
+
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log("MONGODB CONNECTED:", conn.connection.host)
+    } catch (error){
+        console.error("Error while connecting to MONGODB:", error)
+        process.exit(1); // 1 status code means fail and 0 means success
+    }
+}
